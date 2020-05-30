6caf31f839b8beab4be709a6576046acf38a3821
Mohit Sharma <imoisharma@icloud.com>
2020-05-30 20:23:39 +1030

    Adding the intial stage

--- Twitter_Bot/Twitter_Bot.py
+++ Twitter_Bot/Twitter_Bot.py
@@ -1,1 +1,16 @@
+import tweepy
+import time
 
+auth = tweepy.OAuthHandler("Nfk8RBzo9FffsIxZhw9myBgdR","dNVMj4XI0uiBMuvhbMCxaAZisELlHG7HntW3p36LPeH5XiAk88")
+
+auth.set_access_token('1266653208862011392-k36RwrirSY4ePvau93Ru3BYZKSOaR1','phMcrFpLv3av7rZ4Cr21hBJMiMvtmx6wOoTrVjeNrcPL2')
+
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search='devops'
+
+number_of_tweets=100
+
+for tweet in tweepy.Cursor(api.search)
